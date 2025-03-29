1dca7fec0db247a0a6ae2e38ec6b74a7fb9c7440
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-03-29 10:45:06 +0530

    create header component and navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,14 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './component/Header';
 
 export default function App() {
   return (
     
     <BrowserRouter>
+    {/* Header section */}
+    <Header/>
      <Routes>
       <Route path ='/' element={<Home/>}/>
       <Route path ='/about' element={<About/>}/>
